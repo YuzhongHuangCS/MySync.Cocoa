d4bb4f470243c897dde8b093a8c6532894ad7ea9
Yuzhong Huang <yuzhong.huang@innopeaktech.com>
2023-06-20 15:42:31 -0700

    Initial Commit

---
+++ MySync/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MySync
+//
+//  Created by Yuzhong Huang on 6/20/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MySync/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MySync
+//
+//  Created by Yuzhong Huang on 6/20/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
