87890fe6199cc886ba97a4b2ffc3000123f336c5
Yuzhong Huang <yuzhong.huang@innopeaktech.com>
2023-06-21 02:17:15 -0700

    refactor with async

--- MySync/ViewController.swift
+++ MySync/ViewController.swift
@@ -15,62 +15,55 @@ class ViewController: NSViewController {
 
     @IBAction func LoginClicked(_ sender: NSButton) {
         print("LoginClicked")
-
-        guard let presentingWindow = NSApplication.shared.windows.first else {
-          print("There is no presenting window!")
-          return
-        }
-
-        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
-            if error != nil || user == nil {
-                print("Not signed in")
-                GIDSignIn.sharedInstance.signIn(withPresenting: presentingWindow, hint: nil, additionalScopes: ["https://www.googleapis.com/auth/drive"]) { signInResult, error in
-                    guard let signInResult = signInResult else {
-                        print("Error! \(String(describing: error))")
-                        return
-                    }
-                    print("Signin Success")
-                    print(signInResult)
-                    print(signInResult.user.accessToken.tokenString)
-                    print(signInResult.user.profile!.email)
-                    print(GIDSignIn.sharedInstance.currentUser!.accessToken.tokenString)
-                    self.listDirectory(user: signInResult.user)
-                }
-            } else {
-                print("Restore success")
-                print(user!.accessToken.tokenString)
-                print(user!.profile!.email)
-                self.listDirectory(user: user!)
+        Task {
+            if self.service.authorizer == nil {
+                try await initDriveService()
             }
-          }
+
+            try await listDirectory()
+        }
     }
 
-    func listDirectory(user: GIDGoogleUser) {
-        self.service.authorizer = user.fetcherAuthorizer
+    func listDirectory() async throws {
         let query = GTLRDriveQuery_FilesList.query()
-        query.pageSize = 100
-        query.q = "'1DC9vuuiDDMyLccaAH8KAf_Akrk1QQ7Pb' in parents"
         //query.q = "'root' in parents"
-        //query.q = "'root' in parents and mimeType != 'application/vnd.google-apps.folder'"
-        //while true {
-        //    print("here")
-        //}
-        runQuery(query: query)
-    }
-
-    func runQuery(query: GTLRDriveQuery_FilesList) {
-        let raw_query = query.copy() as! GTLRDriveQuery_FilesList
-        self.service.executeQuery(query) { (ticket, result, error) in
-            print(error)
-            let res = result as? GTLRDrive_FileList
-            print(res, "#Files", res!.files?.count)
-            for f in res!.files! {
+        query.q = "'1DC9vuuiDDMyLccaAH8KAf_Akrk1QQ7Pb' in parents"
+        repeat {
+            let result = try await executeQueryAsync(query: query.copy() as! GTLRDriveQuery_FilesList)
+            print(result, "#Files", result.files!.count)
+            for f in result.files! {
                 print(f)
             }
-            if res?.nextPageToken != nil {
-                raw_query.pageToken = res?.nextPageToken
-                self.runQuery(query: raw_query)
+            query.pageToken = result.nextPageToken
+        } while (query.pageToken != nil)
+    }
+
+    func executeQueryAsync(query: GTLRDriveQuery_FilesList) async throws -> GTLRDrive_FileList {
+        try await withCheckedThrowingContinuation { continuation in
+            self.service.executeQuery(query) { (ticket, result, error) in
+                continuation.resume(with: Result<GTLRDrive_FileList, Error> {
+                    if error != nil {
+                        throw error!
+                    }
+                    return result as! GTLRDrive_FileList
+                })
             }
+        }
+    }
+
+    func initDriveService() async throws {
+        if GIDSignIn.sharedInstance.hasPreviousSignIn() {
+            print("Has Previous SignIn")
+            let user = try await GIDSignIn.sharedInstance.restorePreviousSignIn()
+            self.service.authorizer = user.fetcherAuthorizer
+            print(user.profile!.email)
+            print(user.accessToken.expirationDate!)
+            print("Restore Success")
+        } else {
+            print("Run SignIn")
+            let signInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: NSApplication.shared.windows.first!, hint: nil, additionalScopes: ["https://www.googleapis.com/auth/drive"])
+            self.service.authorizer = signInResult.user.fetcherAuthorizer
+            print("SignIn Success")
         }
     }
 
